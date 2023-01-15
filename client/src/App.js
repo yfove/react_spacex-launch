d19e550bfb94bec137c39caa1d45803856914e29
Curtis <curtis.ch.chat@gmail.com>
2023-01-15 12:02:41 -0700

    test API endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [spacexData, setSpacexData] = useState([]);
+
+  const getSpacexData = async () => {
+    const response = await fetch("https://api.spacexdata.com/latest/launches");
+    const data = await response.json();
+    setSpacexData(data);
+  };
+
+  useEffect(() => {
+    getSpacexData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{JSON.stringify(spacexData)}</p>
     </div>
   );
 }
 
 export default App;
+
+// https://github.com/r-spacex/SpaceX-API
