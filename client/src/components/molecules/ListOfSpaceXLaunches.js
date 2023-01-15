1c90073fc12c91d20614c617bd798fb3b36b482b
Curtis <curtis.ch.chat@gmail.com>
2023-01-15 16:00:20 -0700

    filter launches by latest

--- client/src/components/molecules/ListOfSpaceXLaunches.js
+++ client/src/components/molecules/ListOfSpaceXLaunches.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import LaunchCard from "../atoms/LaunchCard.js";
 
 const ListOfSpaceXLaunches = ({ spacexData }) => {
+  const upcomingLaunches = spacexData.filter((launch) => launch.upcoming === true);
+
   return (
     <>
-      {spacexData.length > 0 &&
-        spacexData.map((data) => (
+      {upcomingLaunches.length > 0 &&
+        upcomingLaunches.map((data) => (
           <LaunchCard launchData={data} key={data.flight_numbers} />
         ))}
     </>
